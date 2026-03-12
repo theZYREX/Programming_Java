aa7f686acd4c147e92f52767f6c2e54c1f629c6c
Ilya Ovsyannikov <forzyrex@gmail.com>
2026-03-12 10:47:36 +1000

    Create SolidOfRevolution.java

---
+++ Java Programming - Advanced Course/Lecture_4/Shape/SolidOfRevolution.java
@@ -0,0 +1,13 @@
+public abstract class SolidOfRevolution extends Shape{
+
+    private double radius;
+
+    public SolidOfRevolution(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+}
