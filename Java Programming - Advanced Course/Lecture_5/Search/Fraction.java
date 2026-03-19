4d3bd75d7ff62db39fcc4f5ad3870f063112a2de
Ilya Ovsyannikov <forzyrex@gmail.com>
2026-03-19 12:00:41 +1000

    Create Fraction.java

---
+++ Java Programming - Advanced Course/Lecture_5/Search/Fraction.java
@@ -0,0 +1,36 @@
+public class Fraction implements Comparable<Fraction> {
+
+    public int numerator;
+    public int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public int compareTo(Fraction other) {
+
+        int a = this.numerator;
+        int b = this.denominator;
+
+        int c = other.numerator;
+        int d = other.denominator;
+
+        //a/b - c/d
+
+        int left = a * d;
+        int right = b * c;
+
+        int result = 0;
+
+        if (left>right){result = 1;};
+        if (left < right){result = -1;};
+
+        return result;
+    }
+
+    @Override
+    public String toString() {return numerator + "/" + denominator;}
+
+}
