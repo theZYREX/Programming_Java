f1efab5d16ea8bbbcd2046da32e10b0da6f9238f
Ilya Ovsyannikov <forzyrex@gmail.com>
2026-05-14 11:54:15 +1000

    Create Main.java

---
+++ Java Programming - Advanced Course/Lecture_13/TwoStringOperator/Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TwoStringOperator findLongest = (s1, s2) -> s1.length() > s2.length() ? s1 : s2;
+
+        try {
+            String fullpath = "/Users/ilaovsannikov/прог-е/JavaProg/src/text.txt";
+
+            List<String> allLines = Files.readAllLines(Paths.get(fullpath));
+
+            String longestLine = "";
+
+            for(String currentLine : allLines){
+                longestLine = findLongest.apply(longestLine, currentLine);
+            }
+
+            System.out.println("Самая длинная строка в файле:");
+            System.out.println(longestLine);
+
+        } catch (IOException e) {
+            System.err.println("Ошибка чтения файла: " + e.getMessage());
+        }
+    }
+}
