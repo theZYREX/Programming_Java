3e7b0e9c25d60ee2b93fe6b239fdc6e4951b824a
Ilya Ovsyannikov <forzyrex@gmail.com>
2026-05-14 11:51:51 +1000

    Create booleanOperatorApplication.java

---
+++ Java Programming - Advanced Course/Lecture_13/booleanOperator/booleanOperatorApplication.java
@@ -0,0 +1,46 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class booleanOperatorApplication {
+    public static void main(String[] args) {
+
+        booleanOperator divisibleby13 = (number) -> number % 13 == 0 ? true : false;
+
+        try {
+
+            List<Integer> outputNumbers = new ArrayList<Integer>();
+
+            String Fullpath = "/Users/ilaovsannikov/прог-е/JavaProg/src/input.txt";
+
+            String PathOut = "/Users/ilaovsannikov/прог-е/JavaProg/src/output.txt";
+
+            List<String> lines = Files.readAllLines(Paths.get(Fullpath));
+
+            for (String line : lines) {
+
+                int number = Integer.parseInt(line.trim());
+
+                if(divisibleby13.check(number) == false) {
+                    outputNumbers.add(number);
+                }
+            }
+
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(PathOut))){
+                for (Integer number : outputNumbers) {
+                    writer.write(String.valueOf(number));
+                    writer.newLine();
+                }
+            }
+
+            System.out.println("Числа записаны в файл");
+
+        } catch (IOException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
